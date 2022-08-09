73153a7fc49ffd9e604f437c9a34111571d7871b
lee4339 <skyho4339@naver.com>
2022-08-09 20:01:13 +0900

    프로젝트 작업중2(틀잡기)

---
+++ osullocproject/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import {Route, Link} from 'react-router-dom';
+import MainImg from "./component/view/mainImg"
+import Contents from "./component/view/contents"
+import Footer from "./component/view/footer"
+
+function App() {
+  return (
+  <>
+    <div className='header'>
+      <div className='headerIn'>
+        <div className='leftHeader'>
+          <h1 className='logo'><img src='' alt=''/>로고삽입</h1>
+          <nav className='nav'>
+            <ul>
+              <li className='nav1'><Link to="">제품</Link></li>
+              <li className='nav2'><Link to="">선물추천</Link></li>
+              <li className='nav3'><Link to="">다다익선</Link></li>
+              <li className='nav4'><Link to="">브랜드</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <div className='rightHeader'>
+          <ul className='navUtil'>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <ul className='navDrop'>
+            <li className='navDrop1'>로그인</li>
+            <li className='navDrop2'>
+              KOREAN
+              <span className='DropSpan'>▼</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div>
+      <MainImg/>
+      <Contents/>
+      <Footer/> 
+    </div>
+  </>
+  );
+}
+
+export default App;
